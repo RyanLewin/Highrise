257ca4305ae09a0624aa9de5d683fa335fe2b6ec
Ryan Lewin <ryanjlewin@gmail.com>
2019-06-14 13:41:18 +0100

    Removed old files

--- Vehicles/Vehicle.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Vehicle : MonoBehaviour {
-
-    public int maxPassengers = 4;
-    public int totalPassengers = 0;
-}
